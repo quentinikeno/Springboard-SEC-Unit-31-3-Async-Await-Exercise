ce3fb318cd1648e3bf2c63bec86f1b0ae370d60d
Quentin Ikeno <ikenoq@gmail.com>
2022-04-28 17:46:32 -1000

    Use refactor numberFacts to use async and await.

--- numberFacts.js
+++ numberFacts.js
@@ -3,10 +3,16 @@ const NUMBERS_API_BASE_URL = "http://numbersapi.com/";
 const favNumber = 42;
 const favNumberURL = `${NUMBERS_API_BASE_URL}${favNumber}?json`;
 
-axios
-	.get(favNumberURL)
-	.then((response) => console.log(response.data.text))
-	.catch((err) => console.log(err));
+async function getFavoriteFact() {
+	try {
+		let { data } = await axios.get(favNumberURL);
+		console.log(data.text);
+	} catch (err) {
+		console.log(err);
+	}
+}
+
+getFavoriteFact();
 
 // Figure out how to get data on multiple numbers in a single request. Make that request and when you get the data back, put all of the number facts on the page.
 const fourPromises = Array();
@@ -16,11 +22,18 @@ for (let num of numbers) {
 	fourPromises.push(axios.get(`${NUMBERS_API_BASE_URL}${num}?json`));
 }
 
-Promise.all(fourPromises)
-	.then((respArray) =>
-		respArray.forEach((resp) => console.log(resp.data.text))
-	)
-	.catch((err) => console.log(err));
+async function getFourNumbers() {
+	try {
+		let facts = await Promise.all(fourPromises);
+		for (fact of facts) {
+			console.log(fact.data.text);
+		}
+	} catch (e) {
+		console.log(e);
+	}
+}
+
+getFourNumbers();
 
 // Use the API to get 4 facts on your favorite number. Once you have them all, put them on the page. It’s okay if some of the facts are repeats.
 const fourFavPromises = [];
@@ -30,13 +43,17 @@ for (let i = 0; i < 4; i++) {
 	fourFavPromises.push(axios.get(favNumberURL));
 }
 
-Promise.all(fourFavPromises)
-	.then((respArray) =>
-		respArray.forEach((resp) => {
+async function getFourFacts() {
+	try {
+		let facts = await Promise.all(fourFavPromises);
+		for (fact of facts) {
 			const li = document.createElement("li");
-			li.textContent = resp.data.text;
+			li.textContent = fact.data.text;
 			factList.append(li);
-			console.log(li);
-		})
-	)
-	.catch((err) => console.log(err));
+		}
+	} catch (e) {
+		console.log(e);
+	}
+}
+
+getFourFacts();
