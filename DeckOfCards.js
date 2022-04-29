91719823c4cf672bc5aabf876d978ab38fde4b26
Quentin Ikeno <ikenoq@gmail.com>
2022-04-28 18:25:26 -1000

    Refactor DeckOfCards.js to use async and await.

--- DeckOfCards.js
+++ DeckOfCards.js
@@ -1,75 +1,87 @@
 // Make a request to the Deck of Cards API to request a single card from a newly shuffled deck.
-axios
-	.get("http://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
-	.then((res) =>
-		axios.get(
-			`http://deckofcardsapi.com/api/deck/${res.data.deck_id}/draw/`,
-			{ params: { count: 1 } }
-		)
-	)
-	.then((res) =>
-		console.log(res.data.cards[0].value, " of ", res.data.cards[0].suit)
-	)
-	.catch((err) => console.log(err));
+// axios
+const baseURL = "http://deckofcardsapi.com/api/";
+
+async function drawOneCard() {
+	try {
+		let { data: deckData } = await axios.get(
+			`${baseURL}deck/new/shuffle/?deck_count=1`
+		);
+		const deck_id = deckData.deck_id;
+		let { data } = await axios.get(`${baseURL}deck/${deck_id}/draw`);
+		let { value, suit } = data.cards[0];
+		console.log(value.toLowerCase(), "of", suit.toLowerCase());
+	} catch (e) {
+		console.log(e);
+	}
+}
+
+drawOneCard();
 
 // Make a request to the deck of cards API to request a single card from a newly shuffled deck.
 // Once you have the card, make a request to the same API to get one more card from the same deck.
-axios
-	.get("http://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
-	.then((res) =>
-		axios.get(
-			`http://deckofcardsapi.com/api/deck/${res.data.deck_id}/draw/`,
-			{ params: { count: 1 } }
-		)
-	)
-	.then((res) => {
-		console.log(res.data.cards[0].value, " of ", res.data.cards[0].suit);
-		return axios.get(
-			`http://deckofcardsapi.com/api/deck/${res.data.deck_id}/draw/`,
-			{ params: { count: 1 } }
+
+async function drawTwoCards() {
+	try {
+		let { data: deckData } = await axios.get(
+			`${baseURL}deck/new/shuffle/?deck_count=1`
 		);
-	})
-	.then((res) =>
-		console.log(res.data.cards[0].value, " of ", res.data.cards[0].suit)
-	)
-	.catch((err) => console.log(err));
+		const deck_id = deckData.deck_id;
+		let responses = await Promise.all([
+			await axios.get(`${baseURL}deck/${deck_id}/draw`),
+			await axios.get(`${baseURL}deck/${deck_id}/draw`),
+		]);
+		for (response of responses) {
+			let { data } = response;
+			let { value, suit } = data.cards[0];
+			console.log(value.toLowerCase(), "of", suit.toLowerCase());
+		}
+	} catch (e) {
+		console.log(e);
+	}
+}
+
+drawTwoCards();
 
 // Build an HTML page that lets you draw cards from a deck.
-let deck_id = null;
 const $drawBtn = $("#draw-btn");
 const $drawPile = $("#draw-pile");
 
-axios
-	.get("http://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
-	.then((res) => {
-		deck_id = res.data.deck_id;
+class Deck {
+	constructor() {
+		this.init();
+		this.drawCard = this.drawCard.bind(this);
+	}
+	async init() {
+		let { data } = await axios.get(
+			`${baseURL}deck/new/shuffle/?deck_count=1`
+		);
+		this.deck_id = data.deck_id;
 		$drawBtn.show();
-		$drawBtn.on("click", drawCard);
-	})
-	.catch((err) => console.log(err));
-
-function drawCard() {
-	axios
-		.get(`http://deckofcardsapi.com/api/deck/${deck_id}/draw/`, {
-			params: { count: 1 },
-		})
-		.then((res) => {
-			addCard(res);
-		})
-		.catch((err) => console.log(err));
-}
-
-function addCard(res) {
-	let randAngle = Math.random() * 90 - 45;
-	let src = res.data.cards[0].image;
-	let $img = $("<img>", {
-		src: src,
-		css: {
-			transform: `rotate(${randAngle}deg)`,
-		},
-	});
-	$drawPile.append($img);
-	if (res.data.remaining === 0) {
-		$drawBtn.hide();
+		$drawBtn.on("click", this.drawCard);
+	}
+	async drawCard() {
+		let { data } = await axios.get(
+			`http://deckofcardsapi.com/api/deck/${this.deck_id}/draw/`
+		);
+		this.addCard(data);
+	}
+	addCard(data) {
+		let randomAngle = Math.random() * 90 - 45;
+		let randomX = Math.random() * 40 - 20;
+		let randomY = Math.random() * 40 - 20;
+		let src = data.cards[0].image;
+		let $img = $("<img>", {
+			src: src,
+			css: {
+				transform: `translate(${randomX}px, ${randomY}px) rotate(${randomAngle}deg)`,
+			},
+		});
+		$drawPile.append($img);
+		if (data.remaining === 0) {
+			$drawBtn.hide();
+		}
 	}
 }
+
+new Deck();
